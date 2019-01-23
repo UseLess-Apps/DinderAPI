6a5f0c421f7eaf956455c3132a4962a6e5914c08
Jayden Arquelada <jayarquelada@gmail.com>
2019-01-22 21:14:09 -0500

    Added endpoint for search, return json

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 require('dotenv').config();
 const express = require('express');
-
+const bodyParser = require('body-parser');
 const zomato = require('./papi/zomatoRequests.js');
 
 const app = express();
+app.use(bodyParser.json());
 const port = process.env.PORT;
 
 app.get('/', (req, res) => {
@@ -20,6 +21,26 @@ app.get('/getCuisineTypes', (req, res) => {
         }
     });
 }) 
+
+app.post('/getMatches', (req, res) => {
+    let cuisinesArray = req.body.cuisines;
+    let cuisinesString = '';
+    cuisinesArray.forEach(element => {
+        cuisinesString += `${element},`
+    });
+    cuisinesString = cuisinesString.substring(0, cuisinesString.length - 1);
+    zomato.search(req.query.lat, req.query.long, req.query.start, cuisinesString, (err, res2, body2) => {
+        if (err) {
+            res.status(500).send("Error in zomato Api");
+        }
+        if (res2.statusCode == 200) {
+            console.log(typeof body2);
+            console.log(body2)
+            let restaurants = body2.restaurants;
+            res.send(restaurants)
+        }
+    })
+})
 
 app.listen(port, () => {
     console.log("LISTENING ON PORT " + port)
--- papi/zomatoRequests.js
+++ papi/zomatoRequests.js
@@ -4,16 +4,25 @@ var request = require('request');
 var options = {
     url: process.env.ZOMATO_BASE_URL,
     headers: {
-        'user-key': process.env.ZOMATO_KEY
+        'user-key': process.env.ZOMATO_KEY,
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
     }
 
 }
 
 exports.getCuisines = function getCuisines(lat, long, callback) {
     var req = options;
-    req.url = req.url + "cuisines?lat=${lat}&lon=${long}";
+    req.url = req.url + `cuisines?lat=${lat}&lon=${long}`;
     request(req, (error, res, body) => {
-        callback(error, res, body);
+        callback(error, res, JSON.parse(body));
     });
 }
 
+exports.search = function search(lat, long, start, cuisines, callback) {
+    var req = options;
+    req.url = req.url + `search?lat=${lat}&lon=${long}&start=${start}&cuisines=${cuisines}`;
+    request(req, (error, res, body) => {
+        callback(error, res, JSON.parse(body));
+    })
+}
